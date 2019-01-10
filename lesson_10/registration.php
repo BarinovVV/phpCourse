a2515d0bf31fb1b3425487d277dd6c9ea2f4271a
BarinovVV <ksk05@bk.ru>
2019-01-10 17:02:56 +0300

    Домашнее задание урок 10

--- lesson_10/registration.php
+++ lesson_10/registration.php
@@ -1,8 +1,63 @@
 <?php
-echo "Здесь вы можете зарегистрироваться";
-/**
- * Created by PhpStorm.
- * User: Валерий
- * Date: 10.01.2019
- * Time: 12:09
- */
+//session_start();
+$connection = new PDO('mysql:host=localhost; dbname=academy; charset=utf8', 'root', '');
+$checkLogin = $connection->query('SELECT * FROM login');
+
+
+if ($_POST['newLogin']) {
+    $newLogin = $_POST['newLogin'];
+//    foreach ($checkLogin as $log) {
+//        if ($newLogin == $log['name']) {
+//            echo "<p class='error'>Пользователь с таким логином уже зарегистрирован!</p><br>";
+//            header('Location: registration.php');
+////            die();
+//
+//        }
+//    }
+    $newPassword = $_POST['newPassword'];
+    $newPasswordRepeat = $_POST['newPasswordRepeat'];
+    if ($newPassword === $newPasswordRepeat) {
+        $connection->query("INSERT INTO `login`(`name`, `pass`) VALUES ('$newLogin', '$newPassword')");
+//        session_destroy();
+        header('Location: login.php');
+
+    } else {
+        echo "<p class='error'>Пароли не совпадают!</p><br>";
+//        session_destroy();
+    }
+
+}
+
+?>
+
+<style>
+    body {
+        display: flex;
+        flex-direction: column;
+    }
+    input, p, label, select {
+        font-size: 25px;
+        margin: 10px;
+    }
+    form {
+        order: -1;
+    }
+    .send {
+        width: 300px;
+        height: 55px;
+        background-color: #2aabd2;
+    }
+    .error {
+      color: red;
+    }
+}
+</style>
+
+<form action="#" method="post">
+    <p>Введите ваши регистрационные данные:</p>
+    <input type="text" name="newLogin" required placeholder="Логин"><br>
+    <input type="password" name="newPassword" required placeholder="Пароль"><br>
+    <input type="password" name="newPasswordRepeat" required placeholder="Еще раз пароль"><br>
+
+    <input class="send" type="submit" value="Зарегистроваться">
+</form>
