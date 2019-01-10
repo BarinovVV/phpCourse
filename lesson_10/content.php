26395c8bc6baf0ba36033e82d40c59352666920b
BarinovVV <ksk05@bk.ru>
2019-01-10 13:17:33 +0300

    Домашнее задание урок 10

--- lesson_10/content.php
+++ lesson_10/content.php
@@ -9,11 +9,19 @@ if ($_POST['exit']) {
     session_destroy();
     header('Location: login.php');
 }
-?>
 
-<h1>Эта страница только для авторизованных пользователей</h1>
-<?= "Привет, " . $_SESSION['login'] . "<br>"?>
+?>
+<style>
+  body {
+    background-color: <?=$_SESSION['color'];?>;
+  }
+</style>
+<h1 style="color: #c7254e">Эта страница только для авторизованных пользователей</h1>
+<?= "<h2>Привет, " . $_SESSION['login'] .  "</h2><br>"?>
+<img src="11157811.jpg" alt="img">
+
 
 <form action="" method="post">
-    <input type="submit" name="exit" id="" value="На страницу авторизации">
+    <input style="margin-top: 10px; width: 300px; height: 50px; font-size: 20px; background-color: #eea236;"
+           type="submit" name="exit" id="" value="На страницу авторизации">
 </form>
--- lesson_10/login.php
+++ lesson_10/login.php
@@ -2,17 +2,25 @@
 session_start();
 $connection = new PDO('mysql:host=localhost; dbname=academy; charset=utf8', 'root', '');
 $login = $connection->query('SELECT * FROM login');
-//var_dump($login);
+if ($_SESSION['login']) {
+    header('Location: content.php');
+    die();
 
+}
 if ($_POST['login']) {
     foreach ($login as $log) {
         if ($_POST['login'] == $log['name'] && $_POST['password'] == $log['pass']) {
             $_SESSION['login'] = $_POST['login'];
             $_SESSION['password'] = $_POST['password'];
+            $_SESSION['color'] = $_POST['color'];
             header('Location: content.php');
         }
+
     }
-    echo "Логин или пароль неверные";
+    echo "<p class='error'>Логин или пароль неверные</p><br>";
+//            header('Location: login.php');
+
+    echo "<p class='registration'><a href='registration.php'>Вы можете зарегистрироваться здесь</a></p><br>";
 }
 
 ?>
@@ -21,14 +29,38 @@ if ($_POST['login']) {
     body {
         padding: 10%;
     }
-    input, p {
+    input, p, label, select {
         font-size: 25px;
         margin: 10px;
     }
+    form {
+      text-align: ;
+    }
+    .send {
+      width: 300px;
+      height: 55px;
+      background-color: #2aabd2;
+    }
+    .error {
+      color: red;
+    }
+    .registration a{
+      text-decoration: none;
+    }
 </style>
+
 <form action="#" method="post">
     <p>Авторизуйтесь</p>
     <input type="text" name="login" required placeholder="Логин">
-    <input type="password" name="password" required placeholder="Пароль">
-    <input type="submit">
+    <input type="password" name="password" required placeholder="Пароль"><br>
+    <label for="color">Выберите ваш любимый цвет:</label>
+    <select name="color" id="color">
+      <option value="red">Красный</option>
+      <option value="green">Зеленый</option>
+      <option value="blue">Синий</option>
+      <option value="yellow">Желтый</option>
+      <option value="orange">Оранжевый</option>
+    </select><br>
+    <input class="send" type="submit">
 </form>
+
